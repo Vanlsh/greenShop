c2d8f25b5a96ddaa110f34419176928d45e2b2b3
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-01-07 20:52:53 +0200

    header finish

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const headerBurger = document.querySelector('.header-burger');
+const closeHeaderBurger = document.querySelector('#close-header-burger');
+const openHeaderBurger = document.querySelector('#header-open-burger');
+
+closeHeaderBurger.addEventListener('click', () => {
+  headerBurger.classList.remove('is-open');
+});
+
+openHeaderBurger.addEventListener('click', () => {
+  headerBurger.classList.add('is-open');
+});
